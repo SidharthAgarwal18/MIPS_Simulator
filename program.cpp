a89a98faad3473b3d75c371289d412211aaa3f8c
SidharthAgarwal18 <sidharthagarwal2001@gmail.com>
2021-03-08 19:51:59 +0530

    file changed

--- program.cpp
+++ program.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 using namespace std;
 
-void print_registers(int R[])			//function to print registers.
+void print_registers(int R[])
 {
 	cout<<"$zero :"<<R[0]<<endl<<"$at :"<<R[1]<<endl<<"$v0 :"<<R[2]<<endl<<"$v1 :"<<R[3]<<endl<<"$a0 :"<<R[4]<<endl;
 	cout<<"$a1 :"<<R[5]<<endl<<"$a2 :"<<R[6]<<endl<<"$a3 :"<<R[7]<<endl<<"$t0 :"<<R[8]<<endl<<"$t1 :"<<R[9]<<endl;
@@ -15,7 +15,7 @@ void print_registers(int R[])			//function to print registers.
 	return ;	
 }
 
-int StringtoNumber(string name)		// convert address stored in string to integer
+int StringtoNumber(string name)
 {
 	int number = 0;
 	int digit = 0;
@@ -30,31 +30,35 @@ int StringtoNumber(string name)		// convert address stored in string to integer
 	return number;
 }
 
-int map_instruction(string name,int instruction,int memory[])		//mapping of instructions to unique number
+
+int map_instruction(string name,int instruction,int memory[])
 {
 	int number = 1;
-	if(isSubstring(name,"sub")!=-1)number=2;
-	else if(isSubstring(name,"mul")!=-1)number =3;
-	else if(isSubstring(name,"slt")!=-1)number=4;
-	else if(isSubstring(name,"bne")!=-1)number=5;
-	else if(isSubstring(name,"beq")!=-1)number=6;
-	else if(isSubstring(name,"j")!=-1)number=7;
-	else if(isSubstring(name,"lw")!=-1)number=8;
-	else if(isSubstring(name,"sw")!=-1)number=9;
-	else if(isSubstring(name,"addi")!=-1)number=10;
+	if(name.find("sub")!=string::npos)number=2;
+	else if(name.find("mul")!=string::npos)number =3;
+	else if(name.find("slt")!=string::npos)number=4;
+	else if(name.find("bne")!=string::npos)number=5;
+	else if(name.find("beq")!=string::npos)number=6;
+	else if(name.find("j")!=string::npos)number=7;
+	else if(name.find("lw")!=string::npos)number=8;
+	else if(name.find("sw")!=string::npos)number=9;
+	else if(name.find("addi")!=string::npos)number=10;
 	
 	memory[instruction] = number<<26;			//since only 2^31-1 max value available bits = 30
+	
+	
 	return number;		
+
 }
 
-int strCmp(string s1,string s2)				//checks equality of two strings
+int strCmp(string s1,string s2)
 {
 	if(s1==s2)
-		return true;
-	return false;
+		return 0;
+	return 1;
 }
 
-int R_index(string name)					//mapping of registers to unique number.
+int R_index(string name)
 {
 	if(strCmp(name,"$ze")==0) return 0;
 	else if(strCmp(name,"$at")==0) return 1;
@@ -91,67 +95,65 @@ int R_index(string name)					//mapping of registers to unique number.
 	return -1;
 }
 
-void type_a(string name,int memory[],int instruction)		//for add,sub,mul,slt of form: type $r1,$r2,$r3
+void type_a(string name,int memory[],int instruction)	//for add,sub,mul,slt
 {
 	int pos = name.find("$");
 	string reg = name.substr(pos,3);
-	memory[instruction] = memory[instruction] + R_index(reg)<<21;
-	
+	memory[instruction] = memory[instruction] + (R_index(reg)<<21);
 	pos = name.find("$",pos+3);
 	reg = name.substr(pos,3);
-	memory[instruction] = memory[instruction] + R_index(reg)<<16;
-	
+	memory[instruction] = memory[instruction] + (R_index(reg)<<16);
 	pos = name.find("$",pos+3);
 	reg = name.substr(pos,3);
-	memory[instruction] = memory[instruction] + R_index(reg)<<11;
-	
+	memory[instruction] = memory[instruction] + (R_index(reg)<<11);
+
 	return;	
 }
 
-void type_b(string name,int memory[],int instruction)	//for beq bne addi of form: type $r1,$r2,number
+void type_b(string name,int memory[],int instruction)	//for beq bne addi
 {
 	int pos = name.find("$");
 	string reg = name.substr(pos,3);
-	memory[instruction] = memory[instruction] + R_index(reg)<<21;
+	memory[instruction] = memory[instruction] + (R_index(reg)<<21);
 	
 	pos = name.find("$",pos+3);
 	reg = name.substr(pos,3);
-	memory[instruction] = memory[instruction] + R_index(reg)<<16;
+	memory[instruction] = memory[instruction] + (R_index(reg)<<16);
 	
 	pos = pos + 3;
-	while(name[pos]<48 || name[pos]>57)						//ignoring whitespaces before number.
+	while(name[pos]<48 || name[pos]>57)
 	{pos++;}
-	
 	int address = StringtoNumber(name.substr(pos,name.length()-pos));
 	memory[instruction] = memory[instruction] + address; //address(or no. for addition)is stored in the 16 least sig. bits		
 }
 
-void type_c(string name,int memory[],int instruction)	//for j of the form: j number.
+void type_c(string name,int memory[],int instruction)	//for j
 {
 	int pos = 0;
-	while(name[pos]<48 || name[pos]>57)						//ignoring whitespaces.
+	while(name[pos]<48 || name[pos]>57)
 	{pos++;}
 	
 	int address = StringtoNumber(name.substr(pos,name.length()-pos));
 	memory[instruction] = memory[instruction] + address;       //address is stored in the 26 least significant bits 		
 }
 
-void read_and_save_instruction(string instruction_string,int memory[],int instruction)		//mapping to subcategory
+void read_and_save_instruction(string instruction_string,int memory[],int instruction)
 {
 	int type = map_instruction(instruction_string,instruction,memory);
+	//cout << type ;
 	if(type==1||type==2||type==3||type==4) type_a(instruction_string,memory,instruction);
-	if(type==5||type==6||type==10) type_b(instruction_string,memory,instruction);
+	else if(type==5||type==6||type==10) type_b(instruction_string,memory,instruction);
 	else type_c(instruction_string,memory,instruction);
 }
 
-int decode_a(int memory_instruction,int R[],int instruction,int op)		//decoding type_a
+int decode_a(int memory_instruction,int R[],int instruction,int op)
 {
 	int r3 = ((1<<5)-1) & (memory_instruction>>11);
 	int r2 = ((1<<5)-1) & (memory_instruction>>16);
 	int r1 = ((1<<5)-1) & (memory_instruction>>21);
 	//int op = ((1<<5)-1) & (memory_instruction>>26);
 	
-	if(op==1) R[r1] = R[r2] + R[r3];
+	if(op==1) {R[r1] = R[r2] + R[r3];}
 	else if(op==2) R[r1] = R[r2] - R[r3];
 	else if(op==3) R[r1] = R[r2] * R[r3];
 	else if(op==4 && r2<r3) R[r1] = 1;
@@ -159,7 +161,7 @@ int decode_a(int memory_instruction,int R[],int instruction,int op)		//decoding 
 	return instruction+1;
 }
 
-int decode_b(int memory_instruction,int R[],int instruction,int op)		//decoding type_b
+int decode_b(int memory_instruction,int R[],int instruction,int op)
 {
 	int address = ((1<<16)-1) & (memory_instruction);
 	int r2 = ((1<<5)-1) & (memory_instruction>>16);
@@ -167,9 +169,15 @@ int decode_b(int memory_instruction,int R[],int instruction,int op)		//decoding 
 	//int op = ((1<<5)-1) & (memory_instruction>>26);
 	
 	if(op==6 && r1==r2)
-	{return address;}
+	{
+		//goto address;       // function to jump to required instruction in memory array
+		return address;
+	}
 	else if(op==5 && r1!=r2)
-	{return address;}
+	{
+		//goto address;       // function to jump to required instruction in memory array
+		return address;
+	}
 	else if(op==10) R[r1] = R[r2] + address;
 	return instruction+1;
 }
@@ -177,30 +185,40 @@ int decode_b(int memory_instruction,int R[],int instruction,int op)		//decoding 
  int decode_c(int memory_instruction,int R[],int instruction,int op)
 {
 	int address = ((1<<26)-1) & memory_instruction;
+	// goto address;	// function to jump to required instruction in memory array
 	return address;
 }
 
 int main()
 {
-	int R[32],memory[1<<20];
+	int R[32]={0},memory[1<<20];
 	int instruction = 0;
 	int end_of_instruction = 0; 
 	freopen("input.txt", "r", stdin);
 	freopen("out.txt","w",stdout);
 	string instruction_string;
-	while(cin.getline(instruction_string,50))
+	while(getline(cin,instruction_string))
 	{
 		read_and_save_instruction(instruction_string,memory,instruction);
 		instruction++;
 	}
 	end_of_instruction = instruction;
+	//cout<<" "<<instruction<<"b";
 	instruction = 0;
+	R[8]=9;R[9]=9;R[10] = 145;
+	//cout<<"a ";
+	
+
+
+	print_registers(R);
 	while(instruction<end_of_instruction)
 	{
 		int memory_instruction = memory[instruction];
 		int type = ((1<<5)-1) & (memory_instruction>>26);
+		//cout<<type;
+		//cout<<"a"<<type<<"a";
 		if(type==1||type==2||type==3||type==4) instruction = decode_a(memory_instruction,R,instruction,type);
-		if(type==5||type==6||type==10) instruction = decode_b(memory_instruction,R,instruction,type);
+		else if(type==5||type==6||type==10) instruction = decode_b(memory_instruction,R,instruction,type);
 		else instruction = decode_c(memory_instruction,R,instruction,type);
 		print_registers(R);
 	}
