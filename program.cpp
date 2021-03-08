352bca3babfa86ba3800eb75052af59b40c7cd9b
Mohit Thakur <mohit19082001@gmail.com>
2021-03-08 13:25:34 +0530

    Bit of debugging is still left

--- program.cpp
+++ program.cpp
@@ -1,4 +1,6 @@
-#include<iostream.h>
+#include <iostream>
+#include <bits/stdc++.h>
+#include <string.h>
 using namespace std;
 
 void print_registers(int R[])
@@ -9,14 +11,14 @@ void print_registers(int R[])
 	cout<<"$t7 :"<<R[15]<<endl<<"$s0 :"<<R[16]<<endl<<"$s1 :"<<R[17]<<endl<<"$s2 :"<<R[18]<<endl<<"$s3 :"<<R[19]<<endl;
 	cout<<"$s4 :"<<R[20]<<endl<<"$s5 :"<<R[21]<<endl<<"$s6 :"<<R[22]<<endl<<"$s7 :"<<R[23]<<endl<<"$t8 :"<<R[24]<<endl;
 	cout<<"$t9 :"<<R[25]<<endl<<"$k0 :"<<R[26]<<endl<<"$k1 :"<<R[27]<<endl<<"$gp :"<<R[28]<<endl<<"$sp :"<<R[29]<<endl;
-	cout<<"$fp :"<<R[30]<<endl<<"$ra :"<<R[31]<<endl;
+	cout<<"$fp :"<<R[30]<<endl<<"$ra :"<<R[31]<<endl<<"\n";
 	return ;	
 }
 
 int StringtoNumber(string name)
 {
 	int number = 0;
-	itn digit = 0;
+	int digit = 0;
 	int pos = 0;
 	int n = name.length();
 	while(pos<n && name.at(pos)!=' ' && name.at(pos)!='\n')
@@ -45,55 +47,63 @@ int map_instruction(string name,int instruction,int memory[])
 	return number;		
 }
 
+int strCmp(string s1,string s2)
+{
+	if(s1==s2)
+		return true;
+	return false;
+}
+
 int R_index(string name)
 {
-	if(isSubstring(name,"$zero")!=-1) return 0;
-	else if(isSubstring(name,"$at")!=-1) return 1;
-	else if(isSubstring(name,"$v0")!=-1) return 2;
-	else if(isSubstring(name,"$v1")!=-1) return 3;
-	else if(isSubstring(name,"$a0")!=-1) return 4;
-	else if(isSubstring(name,"$a1")!=-1) return 5;
-	else if(isSubstring(name,"$a2")!=-1) return 6;
-	else if(isSubstring(name,"$a3")!=-1) return 7;
-	else if(isSubstring(name,"$t0")!=-1) return 8;
-	else if(isSubstring(name,"$t1")!=-1) return 9;
-	else if(isSubstring(name,"$t2")!=-1) return 10;
-	else if(isSubstring(name,"$t3")!=-1) return 11;
-	else if(isSubstring(name,"$t4")!=-1) return 12;
-	else if(isSubstring(name,"$t5")!=-1) return 13;
-	else if(isSubstring(name,"$t6")!=-1) return 14;
-	else if(isSubstring(name,"$t7")!=-1) return 15;
-	else if(isSubstring(name,"$s0")!=-1) return 16;
-	else if(isSubstring(name,"$s1")!=-1) return 17;
-	else if(isSubstring(name,"$s2")!=-1) return 18;
-	else if(isSubstring(name,"$s3")!=-1) return 19;
-	else if(isSubstring(name,"$s4")!=-1) return 20;
-	else if(isSubstring(name,"$s5")!=-1) return 21;
-	else if(isSubstring(name,"$s6")!=-1) return 22;
-	else if(isSubstring(name,"$s7")!=-1) return 23;
-	else if(isSubstring(name,"$t8")!=-1) return 24;
-	else if(isSubstring(name,"$t9")!=-1) return 25;
-	else if(isSubstring(name,"$k0")!=-1) return 26;
-	else if(isSubstring(name,"$k1")!=-1) return 27;
-	else if(isSubstring(name,"$gp")!=-1) return 28;
-	else if(isSubstring(name,"$sp")!=-1) return 29;
-	else if(isSubstring(name,"$fp")!=-1) return 30;
-	else if(isSubstring(name,"$ra")!=-1) return 31;
+	if(strCmp(name,"$ze")==0) return 0;
+	else if(strCmp(name,"$at")==0) return 1;
+	else if(strCmp(name,"$v0")==0) return 2;
+	else if(strCmp(name,"$v1")==0) return 3;
+	else if(strCmp(name,"$a0")==0) return 4;
+	else if(strCmp(name,"$a1")==0) return 5;
+	else if(strCmp(name,"$a2")==0) return 6;
+	else if(strCmp(name,"$a3")==0) return 7;
+	else if(strCmp(name,"$t0")==0) return 8;
+	else if(strCmp(name,"$t1")==0) return 9;
+	else if(strCmp(name,"$t2")==0) return 10;
+	else if(strCmp(name,"$t3")==0) return 11;
+	else if(strCmp(name,"$t4")==0) return 12;
+	else if(strCmp(name,"$t5")==0) return 13;
+	else if(strCmp(name,"$t6")==0) return 14;
+	else if(strCmp(name,"$t7")==0) return 15;
+	else if(strCmp(name,"$s0")==0) return 16;
+	else if(strCmp(name,"$s1")==0) return 17;
+	else if(strCmp(name,"$s2")==0) return 18;
+	else if(strCmp(name,"$s3")==0) return 19;
+	else if(strCmp(name,"$s4")==0) return 20;
+	else if(strCmp(name,"$s5")==0) return 21;
+	else if(strCmp(name,"$s6")==0) return 22;
+	else if(strCmp(name,"$s7")==0) return 23;
+	else if(strCmp(name,"$t8")==0) return 24;
+	else if(strCmp(name,"$t9")==0) return 25;
+	else if(strCmp(name,"$k0")==0) return 26;
+	else if(strCmp(name,"$k1")==0) return 27;
+	else if(strCmp(name,"$gp")==0) return 28;
+	else if(strCmp(name,"$sp")==0) return 29;
+	else if(strCmp(name,"$fp")==0) return 30;
+	else if(strCmp(name,"$ra")==0) return 31;
+	return -1;
 }
 
 void type_a(string name,int memory[],int instruction)	//for add,sub,mul,slt
 {
 	int pos = name.find("$");
 	string reg = name.substr(pos,3);
-	memory[instruction] = memory[instruction] + R_index(reg,R)<<21;
+	memory[instruction] = memory[instruction] + R_index(reg)<<21;
 	
 	pos = name.find("$",pos+3);
 	reg = name.substr(pos,3);
-	memory[instruction] = memory[instruction] + R_index(reg,R)<<16;
+	memory[instruction] = memory[instruction] + R_index(reg)<<16;
 	
 	pos = name.find("$",pos+3);
 	reg = name.substr(pos,3);
-	memory[instruction] = memory[instruction] + R_index(reg,R)<<11;
+	memory[instruction] = memory[instruction] + R_index(reg)<<11;
 	
 	return;	
 }
@@ -102,11 +112,11 @@ void type_b(string name,int memory[],int instruction)	//for beq bne addi
 {
 	int pos = name.find("$");
 	string reg = name.substr(pos,3);
-	memory[instruction] = memory[instruction] + R_index(reg,R)<<21;
+	memory[instruction] = memory[instruction] + R_index(reg)<<21;
 	
 	pos = name.find("$",pos+3);
 	reg = name.substr(pos,3);
-	memory[instruction] = memory[instruction] + R_index(reg,R)<<16;
+	memory[instruction] = memory[instruction] + R_index(reg)<<16;
 	
 	pos = pos + 3;
 	while(name[pos]<48 || name[pos]>57)
@@ -126,7 +136,7 @@ void type_c(string name,int memory[],int instruction)	//for j
 	memory[instruction] = memory[instruction] + address;       //address is stored in the 26 least significant bits 		
 }
 
-void read_and_save_instruction(String instruction_string,int memory[],instruction)
+void read_and_save_instruction(string instruction_string,int memory[],int instruction)
 {
 	int type = map_instruction(instruction_string,instruction,memory);
 	if(type==1||type==2||type==3||type==4) type_a(instruction_string,memory,instruction);
@@ -170,9 +180,9 @@ int decode_b(int memory_instruction,int R[],int instruction,int op)
 	return instruction+1;
 }
 
-void decode_c(int memory_instruction,int R[],int instruction,int op)
+ int decode_c(int memory_instruction,int R[],int instruction,int op)
 {
-	int addresss = ((1<<26)-1) & memory[instruction];
+	int address = ((1<<26)-1) & memory_instruction;
 	// goto address;	// function to jump to required instruction in memory array
 	return address;
 }
@@ -183,8 +193,9 @@ int main()
 	int instruction = 0;
 	int end_of_instruction = 0; 
 	freopen("input.txt", "r", stdin);
-	String instruction_string;
-	while(cin.getline(instruction_string,100))
+	freopen("out.txt","w",stdout);
+	string instruction_string;
+	while(cin.getline(instruction_string,50))
 	{
 		read_and_save_instruction(instruction_string,memory,instruction);
 		instruction++;
@@ -193,11 +204,12 @@ int main()
 	instruction = 0;
 	while(instruction<end_of_instruction)
 	{
-		int instruction_string = memory[instruction];
+		int memory_instruction = memory[instruction];
 		int type = ((1<<5)-1) & (memory_instruction>>26);
-		if(type==1||type==2||type==3||type==4) instruction = decode_a(instruction_string,R,instruction,type);
-		if(type==5||type==6||type==10) instruction = decode_b(instruction_string,R,instruction,type);
-		else instruction = decode_c(instruction_string,R,instruction,type);
+		if(type==1||type==2||type==3||type==4) instruction = decode_a(memory_instruction,R,instruction,type);
+		if(type==5||type==6||type==10) instruction = decode_b(memory_instruction,R,instruction,type);
+		else instruction = decode_c(memory_instruction,R,instruction,type);
+		print_registers(R);
 	}
 	return 0;
 }
