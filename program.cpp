9cbf56dd3c647fab99bb2e2b564d14e2a8f1b9a7
Mohit Thakur <mohit19082001@gmail.com>
2021-03-08 11:44:20 +0530

    Almost complete

--- program.cpp
+++ program.cpp
@@ -28,24 +28,24 @@ int StringtoNumber(string name)
 	return number;
 }
 
-void map_instruction(string name,int instruction,int memory[])
+int map_instruction(string name,int instruction,int memory[])
 {
 	int number = 1;
 	if(isSubstring(name,"sub")!=-1)number=2;
 	else if(isSubstring(name,"mul")!=-1)number =3;
-	else if(isSubstring(name,"beq")!=-1)number=4;
+	else if(isSubstring(name,"slt")!=-1)number=4;
 	else if(isSubstring(name,"bne")!=-1)number=5;
-	else if(isSubstring(name,"slt")!=-1)number=6;
+	else if(isSubstring(name,"beq")!=-1)number=6;
 	else if(isSubstring(name,"j")!=-1)number=7;
 	else if(isSubstring(name,"lw")!=-1)number=8;
 	else if(isSubstring(name,"sw")!=-1)number=9;
 	else if(isSubstring(name,"addi")!=-1)number=10;
 	
 	memory[instruction] = number<<26;			//since only 2^31-1 max value available bits = 30
-	return;		
+	return number;		
 }
 
-int R_index(string name,int R[])
+int R_index(string name)
 {
 	if(isSubstring(name,"$zero")!=-1) return 0;
 	else if(isSubstring(name,"$at")!=-1) return 1;
@@ -81,7 +81,7 @@ int R_index(string name,int R[])
 	else if(isSubstring(name,"$ra")!=-1) return 31;
 }
 
-void type_a(string name,int memory[],int R[],int instruction)	//for add,sub,mul,slt
+void type_a(string name,int memory[],int instruction)	//for add,sub,mul,slt
 {
 	int pos = name.find("$");
 	string reg = name.substr(pos,3);
@@ -98,7 +98,7 @@ void type_a(string name,int memory[],int R[],int instruction)	//for add,sub,mul,
 	return;	
 }
 
-void type_b(string name,int memory[],int R[],int instruction)	//for beq bne addi
+void type_b(string name,int memory[],int instruction)	//for beq bne addi
 {
 	int pos = name.find("$");
 	string reg = name.substr(pos,3);
@@ -116,7 +116,7 @@ void type_b(string name,int memory[],int R[],int instruction)	//for beq bne addi
 	memory[instruction] = memory[instruction] + address; //address(or no. for addition)is stored in the 16 least sig. bits		
 }
 
-void type_c(string name,int memory[],int R[],int instruction)	//for j
+void type_c(string name,int memory[],int instruction)	//for j
 {
 	int pos = 0;
 	while(name[pos]<48 || name[pos]>57)
@@ -126,49 +126,78 @@ void type_c(string name,int memory[],int R[],int instruction)	//for j
 	memory[instruction] = memory[instruction] + address;       //address is stored in the 26 least significant bits 		
 }
 
-
-void decode_a(int memory[],int R[],int instruction)
+void read_and_save_instruction(String instruction_string,int memory[],instruction)
 {
-	int r3 = ((1<<5)-1) & (memory[instruction]>>11);
-	int r2 = ((1<<5)-1) & (memory[instruction]>>16);
-	int r1 = ((1<<5)-1) & (memory[instruction]>>21);
-	int op = ((1<<5)-1) & (memory[instruction]>>26);
+	int type = map_instruction(instruction_string,instruction,memory);
+	if(type==1||type==2||type==3||type==4) type_a(instruction_string,memory,instruction);
+	if(type==5||type==6||type==10) type_b(instruction_string,memory,instruction);
+	else type_c(instruction_string,memory,instruction);
+}
+
+int decode_a(int memory_instruction,int R[],int instruction,int op)
+{
+	int r3 = ((1<<5)-1) & (memory_instruction>>11);
+	int r2 = ((1<<5)-1) & (memory_instruction>>16);
+	int r1 = ((1<<5)-1) & (memory_instruction>>21);
+	//int op = ((1<<5)-1) & (memory_instruction>>26);
 	
 	if(op==1) R[r1] = R[r2] + R[r3];
 	else if(op==2) R[r1] = R[r2] - R[r3];
 	else if(op==3) R[r1] = R[r2] * R[r3];
-	else if(op==6 && r2<r3) R[r1] = 1;
-	else R[r1] = 0;	
+	else if(op==4 && r2<r3) R[r1] = 1;
+	else R[r1] = 0;
+	return instruction+1;
 }
 
-void decode_b(int memory[],int R[],int instruction)
+int decode_b(int memory_instruction,int R[],int instruction,int op)
 {
-	int address = ((1<<16)-1) & (memory[instruction]);
-	int r2 = ((1<<5)-1) & (memory[instruction]<<16);
-	int r1 = ((1<<5)-1) & (memory[instruction]<<21);
-	int op = ((1<<5)-1) & (memory[instruction]>>26);
+	int address = ((1<<16)-1) & (memory_instruction);
+	int r2 = ((1<<5)-1) & (memory_instruction>>16);
+	int r1 = ((1<<5)-1) & (memory_instruction>>21);
+	//int op = ((1<<5)-1) & (memory_instruction>>26);
 	
-	if(op==4 && r1==r2)
+	if(op==6 && r1==r2)
 	{
 		//goto address;       // function to jump to required instruction in memory array
+		return address;
 	}
 	else if(op==5 && r1!=r2)
 	{
 		//goto address;       // function to jump to required instruction in memory array
+		return address;
 	}
 	else if(op==10) R[r1] = R[r2] + address;
-		
+	return instruction+1;
 }
 
-void decode_c(int memory[],int R[],int instruction)
+void decode_c(int memory_instruction,int R[],int instruction,int op)
 {
 	int addresss = ((1<<26)-1) & memory[instruction];
 	// goto address;	// function to jump to required instruction in memory array
+	return address;
 }
 
 int main()
 {
 	int R[32],memory[1<<20];
 	int instruction = 0;
-	
+	int end_of_instruction = 0; 
+	freopen("input.txt", "r", stdin);
+	String instruction_string;
+	while(cin.getline(instruction_string,100))
+	{
+		read_and_save_instruction(instruction_string,memory,instruction);
+		instruction++;
+	}
+	end_of_instruction = instruction;
+	instruction = 0;
+	while(instruction<end_of_instruction)
+	{
+		int instruction_string = memory[instruction];
+		int type = ((1<<5)-1) & (memory_instruction>>26);
+		if(type==1||type==2||type==3||type==4) decode_a(instruction_string,R,instruction,type);
+		if(type==5||type==6||type==10) decode_b(instruction_string,R,instruction,type);
+		else decode_c(instruction_string,R,instruction,type);
+	}
+	return 0;
 }
